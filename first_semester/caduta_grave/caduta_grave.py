2e24b53a4bc2f8dd540a045289060e0334cac7f2
samudpa <98691421+samudpa@users.noreply.github.com>
2022-02-01 20:54:54 +0100

    new height data, measured directly from the video

--- first_semester/caduta_grave/caduta_grave.py
+++ first_semester/caduta_grave/caduta_grave.py
@@ -3,12 +3,17 @@ import pandas as pd
 from scipy.optimize import curve_fit
 from draw_plot import plot_time_height
 
-# load data
-position_data = pd.read_csv('data/positions.csv') # position data
-measures_data = pd.read_json('data/measures.json') # tape and ball data
-t = position_data['t']
-h = position_data['h']
-h_err = position_data['h_err']
+# load raw data
+height_data = pd.read_csv('data/height_data.csv') # height data
+measures_data = pd.read_json('data/measures.json', typ='series') # tape and ball data
+
+# convert tape measure to actual height and calculate error
+err_factor = 10 # Maybe too high?
+h = measures_data['tape']['height'] / 100 - height_data['h'] / 100 # [cm] to [m]
+h_err = (height_data['h_delta'] / 100) / err_factor
+
+# convert frame numbers to seconds
+t = (height_data['frame'] - height_data['frame'][0]) / 30 # [s]
 
 # fit model
 def parabola(t, a, v0, h0):
@@ -36,6 +41,6 @@ plot_time_height(
     data = (t, h, h_err, res, popt),
     model = parabola,
     xlim = (-0.035, 0.6),
-    ylim_res = (-0.12, 0.12),
+    ylim_res = (-0.017, 0.017),
     ylim_fit = (-0.15, 1.7)
 )
