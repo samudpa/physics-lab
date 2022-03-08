9ce173d4558cf4e640b3d9b60de731ba45aff489
samudpa <98691421+samudpa@users.noreply.github.com>
2022-03-08 12:50:00 +0100

    improved modularity

--- second_semester/oscillazioni_accoppiate/single_pendulum.py
+++ second_semester/oscillazioni_accoppiate/single_pendulum.py
@@ -1,40 +1,50 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from utils import load_data, find_roots, find_period
+from utils import load_data, find_period
 
-# Part 1: find the angular frequency of a free pendulum and a damped pendulum
+t_err_factor = 1
+pos_err_factor = 1
 
-# free pendulum
-data_0 = load_data('data/single_pendulum.txt')['red']
-pos_0 = data_0['pos'] - np.mean(data_0['pos'])
-T_0, T_0_err, omega_0, omega_0_err = find_period(data_0['t'], pos_0)
+# Part 1: compare the angular frequency of a free pendulum and a damped pendulum
 
-# damped pendulum
-data_d = load_data('data/single_pendulum_damped.txt')['red']
-pos_d = data_d['pos'] - np.mean(data_d['pos'])
-T_d, T_d_err, omega_d, omega_d_err  = find_period(data_d['t'], pos_d)
+data_dicts = []
+for filename in ['data/single_pendulum.txt', 'data/single_pendulum_damped.txt']:
+    data_dict = load_data(
+        filename,
+        fix_offset=True,
+        t_err_factor=t_err_factor,
+        pos_err_factor=pos_err_factor,
+    )
+    data_dicts.append(data_dict['red'])
+
+T_0, T_0_err, omega_0, omega_0_err = find_period(**data_dicts[0])
+T_d, T_d_err, omega_d, omega_d_err = find_period(**data_dicts[1])
 
 print('Angular frequency estimate for free and damped pendulum:')
-print(f'  T_0 [s]\t= {T_0} ± {T_0_err:.2g}')
-print(f'  T_d [s]\t= {T_d} ± {T_d_err:.2g}')
+print(f'  T_0 [s]\t\t= {T_0} ± {T_0_err:.2g}')
+print(f'  T_d [s]\t\t= {T_d} ± {T_d_err:.2g}')
 print(f'  omega_0 [rad/s]\t= {omega_0} ± {omega_0_err:.2g}')
 print(f'  omega_d [rad/s]\t= {omega_d} ± {omega_d_err:.2g}')
 
 # Part 2: find the decay time (tau) of the damped pendulum
 
 def model(t, A, omega, phi, tau):
-    '''Damped pendulum's model'''
-    return A * np.cos(omega * t + phi) * np.exp(- t / tau)
+    '''Damped pendulum model'''
+    return A * np.cos(omega * t + phi) * np.exp(-t / tau)
 
-pos_d_err = np.full(len(pos_d), 1) # 1 mm is the minimum step
-popt, pcov = curve_fit(model, data_d['t'], pos_d, sigma = pos_d_err, p0=(200, 4, -np.pi/4, 25))
+popt, pcov = curve_fit(
+    model,
+    data_dicts[1]['t'],
+    data_dicts[1]['pos'],
+    sigma=data_dicts[1]['pos_err'],
+    p0=(200, 4, -np.pi / 4, 25),
+)
 perr = np.sqrt(np.diag(pcov))
 A_hat, omega_hat, phi_hat, tau_hat = popt
 A_err, omega_err, phi_err, tau_err = perr
 
 print('\nBest fit on the damped pendulum:')
 print(f'  tau [s]\t= {tau_hat} ± {tau_err:.2g}')
-print(f'  A [mm]\t= {A_hat} ± {A_err:.2g}')
+print(f'  A [au]\t= {A_hat} ± {A_err:.2g}')
 print(f'  omega [rad/s]\t= {omega_hat} ± {omega_err:.2g}')
 print(f'  phi [rad]\t= {phi_hat} ± {phi_err:.2g}')
--- second_semester/oscillazioni_accoppiate/utils.py
+++ second_semester/oscillazioni_accoppiate/utils.py
@@ -1,17 +1,13 @@
 import numpy as np
 
-def load_data(filename, cutoff=600):
+def load_raw_data(filename, cutoff=600):
     '''Load time and position data from .txt'''
 
     # load and format data inside a dict
     data = np.loadtxt(filename, unpack=True)
     data_dict = {
-        'red': {
-            't': data[0], 'pos': data[1]
-        },
-        'blue': {
-            't': data[2], 'pos': data[3]
-        }
+        'red': {'t': data[0], 'pos': data[1]},
+        'blue': {'t': data[2], 'pos': data[3]}
     }
 
     # discards the data after the cutoff
@@ -22,7 +18,36 @@ def load_data(filename, cutoff=600):
 
     return data_dict
 
-def find_roots(x,y):
+def load_data(filename, cutoff=600, fix_offset=False, t_err_factor=1, pos_err_factor=1):
+    '''Load time, position data from .txt and calculate the errors'''
+
+    # load data of both pendulums
+    data_raw = load_raw_data(filename, cutoff)
+
+    # iterate over each pendulum
+    result = {}
+    for color in data_raw.keys():
+
+        data = data_raw[color]
+        n = len(data['t'])
+        if fix_offset:
+            data['pos'] -= np.mean(data['pos'])
+
+        # calculate error on time
+        # the error on time is the time between each sample divided by 2
+        t_err = t_err_factor * np.diff(data['t'])[0]
+        data['t_err'] = np.full(n, t_err)
+
+        # calculate error on position
+        possible_values = np.sort(np.unique(data['pos']))
+        pos_err = pos_err_factor * np.min(np.abs(np.diff(possible_values)))
+        data['pos_err'] = np.full(n, pos_err)
+
+        result[color] = data
+
+    return result
+
+def find_roots(x, y):
     '''Find the roots of a set of points'''
     sign = np.sign(y)
     np.place(sign, sign==0, [1])
@@ -30,14 +55,14 @@ def find_roots(x,y):
     indexes = np.concatenate([abs(sign_diff) == 2, [False]])
     return x[indexes]
 
-def find_period(t,pos):
-    '''Find the period of a wave'''
+def find_period(t, pos, t_err, **kwargs):
+    '''Find the period and angular frequency of a wave'''
     roots = find_roots(t, pos)
     t_diff = np.diff(roots)
 
     # period
     T = np.mean(t_diff) * 2
-    T_err = 0.25 * T / np.sqrt(len(t_diff))
+    T_err = t_err[0] / np.sqrt(len(t_diff))
 
     # angular frequency
     omega = 2 * np.pi / T
