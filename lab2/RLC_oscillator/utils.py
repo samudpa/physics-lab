14dda427ce554a0199a4fe39f35474e98a5be6f9
samudpa <98691421+samudpa@users.noreply.github.com>
2023-03-19 20:21:39 +0100

    new data

---
+++ lab2/RLC_oscillator/utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def fmt_measure(value, err, sig=2, sep=" ± "):
+    """Returns a formatted string of (value, err)
+    with the error rounded to `sig` significant figures"""
+
+    decimals = int(sig - np.floor(np.log10(np.abs(err))) - 1)
+    if decimals >= 0:
+        formatter = "{:." + str(decimals) + "f}"
+        return formatter.format(value) + sep + formatter.format(err)
+    if decimals < 0:
+        return f"{int(np.round(value, decimals))}{sep}{int(np.round(err, decimals))}"
